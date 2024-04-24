56a601fb07814b2bb364894c9641460831daa539
Janine Escobillo <janineescobillo@hotmail.com>
2024-04-24 17:41:33 +0200

    adds userSchema

---
+++ models/userSchema.js
@@ -0,0 +1,21 @@
+import mongoose, {Schema, model } from 'mongoose'
+
+const userSchema = new Schema ( {
+  email: {
+    type: String, 
+    required: true, 
+    unique: true
+  },
+  passwordHash: {
+    type: String,
+    required: true
+  },
+  apiKey: {
+    type: String,
+    required: true,
+  }
+}, { timestamps: true })
+
+const User = mongoose.model.User || model ('User', userSchema)
+
+export default User;
