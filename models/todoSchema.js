b921caa1cfc6e3580e71f3f51fdd3b775b47d1db
Janine Escobillo <janineescobillo@hotmail.com>
2024-04-24 17:28:07 +0200

    adds todoSchema

---
+++ models/todoSchema.js
@@ -0,0 +1,19 @@
+import  { Schema, model } from 'mongoose'
+
+const todoSchema = new Schema ({
+  title: {
+    type: String,
+    required: true
+  },
+  completed: {
+    type: Boolean,
+    default: false
+  },
+  userId: {
+    type: Schema.Types.ObjectId,
+    ref: 'User',
+    required: true
+  }
+}, { timestamps: true })
+
+const Todo = mongoose.models.Todo || model ('Todo', todoSchema)
